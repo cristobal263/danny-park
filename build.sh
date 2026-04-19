cd07b993b2c593fdb96c806e9bab16871d031263
cristobal263 <cristobalpatino09@gmail.com>
2026-04-19 01:52:37 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
-# Salir si hay un error
 set -o errexit
-
-# Instalar las librerías necesarias (Django, etc.)
 pip install -r requirements.txt
-
-# Recolectar archivos de diseño (CSS, imágenes)
 python manage.py collectstatic --no-input
-
-# Actualizar la base de datos en el servidor de Render
 python manage.py migrate
